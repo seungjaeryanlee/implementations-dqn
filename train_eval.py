4bdedf1541d904d42660085d93ab88be8a0eb87a
Seung Jae (Ryan) Lee <seungjaeryanlee@gmail.com>
2019-07-21 21:12:08 +0900

    Separate utility functions

--- train_eval.py
+++ train_eval.py
@@ -54,7 +54,6 @@ rew : Reward
 import copy
 import os
 import random
-from typing import Callable
 
 import configargparse
 import gym
@@ -66,34 +65,7 @@ import torch.optim as optim
 from common import get_logger, make_reproducible
 from networks import QNetwork
 from replays import ReplayBuffer, Transition
-
-
-def get_linear_anneal_func(
-    start_value: float, end_value: float, end_steps: int
-) -> Callable:
-    """Create a linear annealing function.
-
-    Parameters
-    ----------
-    start_value : float
-        Initial value for linear annealing.
-    end_value : float
-        Terminal value for linear annealing.
-    end_steps : int
-        Number of steps to anneal value.
-
-    Returns
-    -------
-    linear_anneal_func : Callable
-        A function that returns annealed value given a step index.
-
-    """
-
-    def linear_anneal_func(x):
-        assert x >= 0
-        return (end_value - start_value) * min(x, end_steps) / end_steps + start_value
-
-    return linear_anneal_func
+from utils import get_linear_anneal_func
 
 
 def select_action(
