992b5d5f5536171392a9832356446f10b8cd200c
Seungjae Ryan Lee <seungjaeryanlee@gmail.com>
2019-07-29 05:55:53 +0900

    Create AtariQNetwork

--- dqn/networks.py
+++ dqn/networks.py
@@ -40,3 +40,45 @@ class QNetwork(nn.Module):
 
         """
         return self.layers(x)
+
+
+class AtariQNetwork(nn.Module):
+    def __init__(self, in_dim: int, out_dim: int):
+        """Initialize Q-Network for Atari environments.
+
+        Parameters
+        ----------
+        in_dim : int
+            Number of stacked frames.
+        out_dim : int
+            Number of actions.
+
+        """
+        super().__init__()
+        self.conv_layers = nn.Sequential(
+            nn.Conv2d(in_dim, 32, 8, stride=4, padding=0),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, 4, stride=2, padding=0),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, 3, stride=1, padding=0),
+            nn.ReLU(),
+        )
+        self.fc_layers = nn.Sequential(
+            nn.Linear(in_dim, 512), nn.ReLU(), nn.Linear(512, out_dim)
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """Forward propagation.
+
+        Parameters
+        ----------
+        x : torch.Tensor
+            Input tensor for the network. Should be the observation.
+
+        Returns
+        -------
+        q_values : torch.Tensor
+            Output tensor of the network. Q-values of all actions.
+
+        """
+        return self.fc_layers(self.conv_layers(x))
